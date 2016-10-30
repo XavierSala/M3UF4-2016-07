ef8f2138971273fe5c16f76e8ec8a503bd0769b5
Xavier Sala <utrescu@gmail.com>
2016-10-30 22:31:19 +0100

    Comentaris

--- src/main/java/net/xaviersala/Comarca.java
+++ src/main/java/net/xaviersala/Comarca.java
@@ -42,18 +42,33 @@ public class Comarca {
 		return rectangle.getBounds().intersects(cavallerRect);
 	}
 
+	/**
+	 * 
+	 * @return Obtenir el cavaller que domina la casella. 
+	 */
 	public Cavaller getCavaller() {
 		return propietari;
 	}
 
+	/**
+	 * El cavaller ara domina la comarca.
+	 * 
+	 * @param cavaller Cavaller dominador
+	 */
 	public void setCavaller(Cavaller cavaller) {
 		this.propietari = cavaller;		
 	}
 	
+	/**
+	 * @return Determina si la casella és un castell o no.
+	 */
 	public boolean isCastell() {
 		return castell;
 	}
 
+	/**
+	 * Converteix la casella en un castell.
+	 */
 	public void setCastell() {
 		castell = true;		
 		// TODO: Eliminar-lo quan hi hagi imatge
@@ -61,6 +76,13 @@ public class Comarca {
 		
 	}
 
+	/**
+	 * Mira si el cavaller està ocupant alguna casella i retorna els cavallers
+	 * usats.
+	 * 
+	 * @param cavaller cavaller que conquereix
+	 * @return número de castells conquerits
+	 */
 	public boolean ocupadaPer(Cavaller cavaller) {
 		if (cavaller == propietari) {
 			return false;

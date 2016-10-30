b7f70f13118c58e7fc200841540b3345c8360a58
Xavier Sala <utrescu@gmail.com>
2016-10-30 22:15:19 +0100

    Primer intent

--- src/main/java/net/xaviersala/App.java
+++ src/main/java/net/xaviersala/App.java
@@ -1,7 +1,16 @@
 package net.xaviersala;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
+import acm.graphics.GPoint;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+import net.xaviersala.conqueridor.Cavaller;
+import net.xaviersala.conqueridor.Comte;
 
 /**
  * Plantilla base per fer un programa fent servir les llibreries
@@ -10,22 +19,90 @@ import acm.program.GraphicsProgram;
  */
 public class App extends GraphicsProgram
 {
-    /**
+	/**
     *
     */
    private static final long serialVersionUID = 1299094805237490891L;
    private static final int AMPLADAPANTALLA = 1024;
    private static final int ALTURAPANTALLA = 768;
+   private static final double AMPLE = 40;
+   private static final double ALT = 40;
+   private static final int AMPLEMAPA = 25;
+   private static final int ALTMAPA = 15;
+   
+   private static final String[] NOMS = { "Hug", "Ramon", "Bernat" };
 
+
+    Mapa mapa;
     /**
      * Programa principal...
      */
     @Override
     public final void run() {
        setSize(AMPLADAPANTALLA, ALTURAPANTALLA);
-
+       
+       mapa = new Mapa("Quadrilàndia", generarCaselles(5, 5));
+       
+       mapa.afegirComtes(crearComtes());
+       
        clicaPerComencar();
     }
+
+
+
+
+
+private List<Comte> crearComtes() {
+		List<Comte> comptes = new ArrayList<>();
+		for(String nom: NOMS) {
+			// GImage imatgeCompte = new GImage(nom + ".png");
+			GPoint lloc = mapa.getRandomCastell();
+			Comte comte = new Comte(null, lloc);			
+			
+			for (int i=0; i<2; i++) {
+				GImage imatgeCavaller = new GImage("cavaller.png");
+				imatgeCavaller.setLocation(lloc);
+				add(imatgeCavaller);
+				comte.afegirCavaller(new Cavaller(nom + i, imatgeCavaller, comte));
+			}
+		}
+		return comptes;
+	}
+
+
+
+
+
+private List<Comarca> generarCaselles(int xInicial, int yInicial) {
+	
+		Random aleatori = new Random(); 
+		
+		List<Comarca> comarques = new ArrayList<Comarca>(xInicial*yInicial);
+
+		int posx = xInicial;
+		int posy = yInicial;
+
+		for(int i=0; i < ALTMAPA; i++) {
+			for(int j=0; j < AMPLEMAPA; j++) {
+				GRect posicio = new GRect(posx, posy, AMPLE, ALT);
+				add(posicio);
+				Comarca comarca = new Comarca(posicio, null, false);
+				comarques.add(comarca);
+				posx += AMPLE;
+			}			
+			posy += ALT;
+			posx = xInicial;
+		}
+		
+		
+		for(int i=0; i<6; i++) {
+			int quina = aleatori.nextInt(comarques.size());
+			comarques.get(quina).setCastell();
+		}
+		
+		
+		return comarques;
+	}
 
 
 
---
+++ src/main/java/net/xaviersala/Comarca.java
@@ -0,0 +1,88 @@
+package net.xaviersala;
+
+import java.awt.Color;
+
+import acm.graphics.GImage;
+import acm.graphics.GPoint;
+import acm.graphics.GRect;
+import acm.graphics.GRectangle;
+import net.xaviersala.conqueridor.Cavaller;
+import net.xaviersala.conqueridor.Comte;
+
+public class Comarca {
+	
+	GRect rectangle;
+	GImage imatgeCastell;
+	boolean castell;
+	Cavaller propietari;
+	
+	public Comarca(GRect rectangle, GImage castell, boolean teCastell) {
+		
+		this.rectangle = rectangle;
+		this.rectangle.setFilled(true);
+		this.rectangle.setFillColor(Color.WHITE);
+		
+		this.imatgeCastell = castell;
+		this.castell = teCastell;		
+		
+		this.propietari = null;
+	}
+	
+	public Comte deQuinCompte() {
+		if (propietari == null) {
+			return null;
+		}
+		else {
+			return propietari.getComte();
+		}
+	}
+
+	// Diu si el rectangle del cavaller toca amb la casella...
+	public boolean xocaAmb(GRectangle cavallerRect) {
+		return rectangle.getBounds().intersects(cavallerRect);
+	}
+
+	public Cavaller getCavaller() {
+		return propietari;
+	}
+
+	public void setCavaller(Cavaller cavaller) {
+		this.propietari = cavaller;		
+	}
+	
+	public boolean isCastell() {
+		return castell;
+	}
+
+	public void setCastell() {
+		castell = true;		
+		// TODO: Eliminar-lo quan hi hagi imatge
+		rectangle.setFillColor(Color.BLACK);
+		
+	}
+
+	public boolean ocupadaPer(Cavaller cavaller) {
+		if (cavaller == propietari) {
+			return false;
+		}
+		
+		if (rectangle.getBounds().intersects(cavaller.getPosicio())) {
+			// Canvia el propietari
+			propietari = cavaller;
+			rectangle.setFillColor(propietari.getColor());
+			return (castell);
+		}
+				
+		return false;
+	}
+
+	public boolean isDelCavaller(Cavaller cavaller) {		
+		return (propietari == cavaller);
+	}
+
+	public GPoint getPosicio() {
+		return rectangle.getLocation();
+	}
+
+
+}
